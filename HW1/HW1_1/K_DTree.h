bb74bf8ede2211aaf4f8b3d72a168ab55b9021f1
david <david11014@gmail.com>
2017-10-05 17:25:10 +0800

    小修改

---
+++ HW1/HW1_1/K_DTree.h
@@ -0,0 +1,60 @@
+#ifndef K_DTREE
+#define K_DTREE
+
+class Point2D {
+
+public:
+	union
+	{
+		double p[2];
+		struct
+		{
+			double x;
+			double y;
+		};
+	};
+	int l;
+
+	double& operator[](unsigned int i)
+	{
+		return p[i];
+	}
+
+};
+
+
+class node {
+
+public:
+	node* up;
+	node* left;
+	node* right;
+	Point2D* P;
+	node() {
+		left = nullptr;
+		right = nullptr;
+		P = nullptr;
+	}
+	node(node* u)
+	{
+		left = nullptr;
+		right = nullptr;
+		P = nullptr;
+
+		up = u;
+	}
+};
+
+class KDTree {
+	
+	node Root;
+
+	void add(node* pa, Point2D * P)
+	{
+		
+	}
+
+};
+
+#endif // !K_DTREE
+
